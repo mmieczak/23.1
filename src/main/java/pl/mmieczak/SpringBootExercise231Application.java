69672d53aa4f8f2cefdd7613f4c0a6e2688b8fec
Maciej Mieczakowski <maciej.mieczakowski@gmail.com>
2020-06-30 16:39:43 +0200

    Empty project

---
+++ src/main/java/pl/mmieczak/SpringBootExercise231Application.java
@@ -0,0 +1,13 @@
+package pl.mmieczak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExercise231Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExercise231Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mmieczak/SpringBootExercise231ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mmieczak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExercise231ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
